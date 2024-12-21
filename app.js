fbe70d31331dcfd789a28f0ed1fc18ab9ecda1ff
MuzammilBaig10 <mirzabaig4689@gmail.com>
2024-12-21 12:30:07 +0500

    update

---
+++ app.js
@@ -0,0 +1,63 @@
+// // // // var list = []
+
+// // // // // var x = prompt("fruits or colors")
+
+// // // // // if (x == "fruits") {
+
+// // // // //     list.push("apple,orange,mango")
+// // // // //     alert(list) 
+
+// // // // // } else if (x == "colors") {
+
+// // // // //     list.push("red,black,orange")
+// // // // //     alert(list)
+
+// // // // // } else {
+
+// // // // //     alert("invalid input")
+
+// // // // // }
+
+// // // var fruits = ["apple" , "banana" , "grapes" , "peach"]
+// // // console.log ("My Fruits :" , fruits)
+// // // fruits.shift ()
+// // // console.log ("My Fruits After Shift() :" , fruits)
+
+// fruits = ["apple" , "banana" , "grapes" , "peach"]
+// console.log ("My Fruits :" ,fruits)
+// // fruits.splice(3,1)
+// // console.log ("My Fruits After Splice" , fruits)
+
+var firstNames = ["abubakar", "umer", "usman", "ali", "muhammad"];
+var lastNames = ["abdullah", "ahmed", "daniyal"]
+
+for (var i = 0; i < 5; i++) {
+
+
+    for (var j = 0; j < 3; j++) {
+        console.log(firstNames[i] + " " + lastNames[j])
+    }
+
+    if (i == 0) {
+        console.log("FIRST LOOP ITERATION COMPLETE");
+    }
+
+    else if (i == 1) {
+        console.log("SECOND LOOP ITERATION COMPLETE");
+    }
+
+    else if (i == 2) {
+        console.log("THIRD LOOP ITERATION COMPLETE");
+    }
+
+    else if (i == 3) {
+        console.log("FOURTH LOOP ITERATION COMPLETE");
+    }
+
+    else if (i == 4) {
+        console.log("FIFTH LOOP ITERATION COMPLETE");
+    }
+
+
+
+}
