abb8b843308cea55229b573fe7caed06b1e25204
MuzammilBaig10 <mirzabaig4689@gmail.com>
2024-12-21 12:48:47 +0500

    update 2

--- app.js
+++ app.js
@@ -28,36 +28,41 @@
 // // fruits.splice(3,1)
 // // console.log ("My Fruits After Splice" , fruits)
 
-var firstNames = ["abubakar", "umer", "usman", "ali", "muhammad"];
-var lastNames = ["abdullah", "ahmed", "daniyal"]
+// var firstNames = ["abubakar", "umer", "usman", "ali", "muhammad"];
+// var lastNames = ["abdullah", "ahmed", "daniyal"]
 
-for (var i = 0; i < 5; i++) {
+// for (var i = 0; i < 5; i++) {
 
 
-    for (var j = 0; j < 3; j++) {
-        console.log(firstNames[i] + " " + lastNames[j])
+//     for (var j = 0; j < 3; j++) {
+//         console.log(firstNames[i] + " " + lastNames[j])
+//     }
+
+//     if (i == 0) {
+//         console.log("FIRST LOOP ITERATION COMPLETE");
+//     }
+
+//     else if (i == 1) {
+//         console.log("SECOND LOOP ITERATION COMPLETE");
+//     }
+
+//     else if (i == 2) {
+//         console.log("THIRD LOOP ITERATION COMPLETE");
+//     }
+
+//     else if (i == 3) {
+//         console.log("FOURTH LOOP ITERATION COMPLETE");
+//     }
+
+//     else if (i == 4) {
+//         console.log("FIFTH LOOP ITERATION COMPLETE");
+//     }
+// }
+
+for (var i = 0; i<=15; i++) {
+    if (i % 2 === 0) {
+        console.log (i + " is even");
+    } else {
+        console.log(i + " is odd");
     }
-
-    if (i == 0) {
-        console.log("FIRST LOOP ITERATION COMPLETE");
-    }
-
-    else if (i == 1) {
-        console.log("SECOND LOOP ITERATION COMPLETE");
-    }
-
-    else if (i == 2) {
-        console.log("THIRD LOOP ITERATION COMPLETE");
-    }
-
-    else if (i == 3) {
-        console.log("FOURTH LOOP ITERATION COMPLETE");
-    }
-
-    else if (i == 4) {
-        console.log("FIFTH LOOP ITERATION COMPLETE");
-    }
-
-
-
 }
