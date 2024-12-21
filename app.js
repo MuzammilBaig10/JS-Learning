6100428e8eba725353203174916343ed0f6867bc
MuzammilBaig10 <mirzabaig4689@gmail.com>
2024-12-21 12:51:11 +0500

    update

--- app.js
+++ app.js
@@ -59,10 +59,11 @@
 //     }
 // }
 
-for (var i = 0; i<=15; i++) {
-    if (i % 2 === 0) {
-        console.log (i + " is even");
-    } else {
-        console.log(i + " is odd");
-    }
-}
+// for (var i = 0; i<=15; i++) {
+//     if (i % 2 === 0) {
+//         console.log (i + " is even");
+//     } else {
+//         console.log(i + " is odd");
+//     }
+// }
+
